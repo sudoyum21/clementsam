64964e2484873cdc49b3f149586eeac08914e2ca
sudoyum21 <ba.samson.lam@gmail.com>
2017-10-06 15:10:16 -0400

    foreach

--- tp3/LOG4420_TP3/assets/scripts/products.js
+++ tp3/LOG4420_TP3/assets/scripts/products.js
@@ -3,28 +3,27 @@ var getProducts = (function () {
 	var rawData;
 	
 	function _getAsync(){
-		$.get("http://localhost:8000/data/products.json", (data) => {
+		$.get("./data/products.json", (data) => {
 			rawData = data;
 			trier(data);		
 		});
 	}
 	function trier(data){
 		//trier tableau avewc $
-		alert(data)
-		alert($("#products-list"))
 		var id = "";
-		for(var attr in data){
-			id=data[attr].id;
-			$("#products-list").append('		<div class="product">	'+
-			'		<a href="./product.html" title="En savoir plus...">	'+
-			'	<h2>'+id+'</h2>		 '+
-			'	<img alt="Manette Xbox 360" '+
+
+		data.forEach((element) => {
+			$("#products-list").append(
+				'		<div class="product">	'+
+				'		<a href="./product.html" title="En savoir plus...">	'+
+				'	<h2>'+element.id+'</h2>		 '+
+				'	<img alt="Manette Xbox 360" '+
 				'src="./assets/img/xbox-controller.png">		 '+
 				'<p><small>Prix</small> 29,99&thinsp;$</p>		'+
 				'</a>'+
-						'</div>')
-		}
-
+				'</div>')
+			});
+		
 
 
 

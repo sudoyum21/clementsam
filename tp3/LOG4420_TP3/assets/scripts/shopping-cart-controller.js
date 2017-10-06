8bd9d81a974a1006bbac9990ff5381276c316c91
sudoyum21 <ba.samson.lam@gmail.com>
2017-10-06 14:28:52 -0400

    prep merge

--- tp3/LOG4420_TP3/assets/scripts/shopping-cart-controller.js
+++ tp3/LOG4420_TP3/assets/scripts/shopping-cart-controller.js
@@ -21,8 +21,31 @@ var example = (function (number, document) {
     };
 });
 console.log($(".logo"))
+let testA = new example(5, document);
+console.log(testA.publicFunction2());
+console.log(testA)
+let testB = new example(12, window.document);
+
+console.log(testB.publicFunction2());
+console.log(testB.publicFunction2())
+console.log($(".count").text());
 
 
+function setBadge(){
+    let rowCount = $('table.table >tbody >tr').length;
+    if(rowCount){
+        $(".count").css("visibility", "visible");
+        $(".count").text(rowCount);
+    } else {
+        $(".count").css("visibility", "hidden");
+    }
+}
+
+setBadge();  
 
 
+// var rowCount = $('table.table >tbody >tr').each(function(idx) {
+//     console.log(this)
+//     console.log(idx)
+// }, this);;
 

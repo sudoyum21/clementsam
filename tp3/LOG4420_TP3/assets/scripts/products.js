d1bd4f9d38058201294a06aa56c1551b95615d7b
Flyxo <flypi.flopi.flap@gmail.com>
2017-10-06 16:10:16 -0400

    products js: toutes les fonctions de tri. A organiser

--- tp3/LOG4420_TP3/assets/scripts/products.js
+++ tp3/LOG4420_TP3/assets/scripts/products.js
@@ -1,41 +1,74 @@
+'use strict'
+/* Besoin de séparer services et controllers (controllers = touche au html)
+   Clic catégorie: supprimer tout le html de produtcs-list et rajouter ceux qu'il faut
+*/
 var getProducts = (function () {
-	var rawData;
-	
-	function _getAsync(){
-		$.get("./data/products.json", (data) => {
-			rawData = data;
-			trier(data);		
+	function _getAsync() {
+		$.get("./data/products.json", data => {
+			//data = computersOnly(data);
+			sortPriceLowHigh(data);
+			displayProducts(data);
 		});
-	}
-	function trier(data){
-		//trier tableau avewc $
-		var id = "";
-
-		data.forEach((element) => {
-			$("#products-list").append(
-				'		<div class="product">	'+
-				'		<a href="./product.html" title="En savoir plus...">	'+
-				'	<h2>'+element.id+'</h2>		 '+
-				'	<img alt="Manette Xbox 360" '+
-				'src="./assets/img/xbox-controller.png">		 '+
-				'<p><small>Prix</small> 29,99&thinsp;$</p>		'+
-				'</a>'+
-				'</div>')
-			});
-		
-
-
-
-	
-	}
+	};
+	function camerasOnly(data) {
+		return data.filter(product => {
+			return product.category === 'cameras';
+		});
+	};
+	function consolesOnly(data) {
+		return data.filter(product => {
+			return product.category === 'consoles';
+		});
+	};
+	function screensOnly(data) {
+		return data.filter(product => {
+			return product.category === 'screens';
+		});
+	};
+	function computersOnly(data) {
+		return data.filter(product => {
+			return product.category === 'computers';
+		});
+	};
+	function sortPriceLowHigh(data) {
+		data.sort((a, b) => {
+			return a.price > b.price;
+		});
+	};
+	function sortPriceHighLow(data) {
+		data.sort((a, b) => {
+			return a.price < b.price;
+		});
+	};
+	function sortNameAZ(data) {
+		data.sort((a, b) => {
+			return a.name > b.name;
+		});
+	};
+	function sortNameZA(data) {
+		data.sort((a, b) => {
+			return a.name < b.name;
+		});
+	};
+	function getProductTemplate(name, image, price) {
+		return '        <div class="product">' +
+		'		   <a href="./product.html" title="En savoir plus...">' +
+		'	         <h2>' + name + '</h2>' +
+		'	         <img alt="' + name + '" src="./assets/img/' + image + '">' +
+		'            <p><small>Prix</small> ' + price + '&thinsp;$</p>' +
+		'          </a>' +
+		'        </div>';
+	};
+	function displayProducts(data) {
+		data.forEach(product => {
+			$("#products-list").append(getProductTemplate(product.name, product.image, product.price));
+		});
+	};
 	return {       
 		getDatas: function () {
             _getAsync();
         },
     };
 });
-
-var test = getProducts();
+var test = new getProducts();
 test.getDatas();
-console.log($.get("http://localhost:8000/data/products.json", (data) => {	
-}))

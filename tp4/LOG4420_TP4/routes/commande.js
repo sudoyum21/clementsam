cb23e7227319521467584b74d315a09f2e62f6a3
Flyxo <flypi.flopi.flap@gmail.com>
2017-11-10 15:07:09 -0500

    add pug files

--- tp4/LOG4420_TP4/routes/commande.js
+++ tp4/LOG4420_TP4/routes/commande.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-//127;;..8000:/cmommande/
+
 router.get("/", function(req, res) {
   res.render("index", { title: "COMMANDE", message: "Ça semble fonfdsfsdfctionner!" });
 });

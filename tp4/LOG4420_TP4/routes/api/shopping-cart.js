882617217e75e17c1f01c7a9cedcf92be7c43b32
Flyxo <flypi.flopi.flap@gmail.com>
2017-11-16 15:00:29 -0500

    ?

--- tp4/LOG4420_TP4/routes/api/shopping-cart.js
+++ tp4/LOG4420_TP4/routes/api/shopping-cart.js
@@ -11,43 +11,24 @@ router.get("/", function (req, res) {
     }
     mongoose.connection.db.collection('products', function (err, collection) {
         console.log(collection.find({}));
-        });
+    });
     res.render("index", { title: "COMMANDE", message: "Ça semble extra!"+id });
     //Get the default connection
     // var db = mongoose.connection;
     // //Bind connection to error event (to get notification of connection errors)
     // db.on('error', console.error.bind(console, 'MongoDB connection error:'));
     //mongoose.
-  
-  
+
+
     // doc.find({}, function (err, collection) {
     //   console.log(collection)
-    //   console.log(collection)
-    //   console.log(collection)
-    //   console.log(collection)
-    //   console.log(collection)
     // });
-  
+
   });
 
 router.get("/:productId", function (req, res) {
     var id = req.params.productId;
   res.render("index", { title: "COMMANDE", message: "Ça semble 5555!"+id });
-  //Get the default connection
-  // var db = mongoose.connection;
-  // //Bind connection to error event (to get notification of connection errors)
-  // db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-  //mongoose.
-
-
-  // doc.find({}, function (err, collection) {
-  //   console.log(collection)
-  //   console.log(collection)
-  //   console.log(collection)
-  //   console.log(collection)
-  //   console.log(collection)
-  // });
-
 });
 
 module.exports = router;
